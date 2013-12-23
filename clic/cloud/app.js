596d327f8c28ce2d5f0e4e15c8fcef3463100a02
mduppes <mduppes@fb.com>
2013-12-23 00:23:39 -0800

    fix static js files, fix login app id

--- clic/cloud/app.js
+++ clic/cloud/app.js
@@ -10,8 +10,8 @@ app.use(express.bodyParser());    // Middleware for reading request body
 
 // This is an example of hooking up a request handler with a specific request
 // path and HTTP verb using the Express routing API.
-app.get('/hello', function(req, res) {
-  res.render('index', { message: 'Congrats, you just set up your app!' });
+app.get('/', function(req, res) {
+  res.render('index', {});
 });
 
 app.get('/make', function(req, res) {
---
+++ clic/public/js/main.js
@@ -0,0 +1,12 @@
+
+
+
+
+var Utils = {
+
+  facebookLogin: function(user, pass) {
+  }
+
+
+
+};
